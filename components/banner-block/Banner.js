a5cd37c8e958af519889d4b812f82977476c3bdc
Denys Bazanov <dbazanov@deloitte.com>
2022-03-04 21:59:19 +0200

    simple parallax

--- components/banner-block/Banner.js
+++ components/banner-block/Banner.js
@@ -1,67 +1,89 @@
 import styles from  './Banner.module.scss';
 import LinkButton from '../link-button/LinkButton';
 import Timer from '../timer/Timer';
+import React from 'react';
 
 const targetDate = new Date('2022-03-15');
 
-function Banner() {
-  return (
-    <div className={styles.Banner}>
-      <div className={styles.header}>
-        <a
-          href='https://savelife.in.ua/'
-          target='_blank'
-          rel='noreferrer'>
+class Banner extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollOffset: 0,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => {
+      const { scrollY, innerHeight } = window;
+      this.setState({
+        scrollOffset: (scrollY / innerHeight) * 30,
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className={styles.Banner}>
+        <div className={styles.header}>
+          <a
+            href='https://savelife.in.ua/'
+            target='_blank'
+            rel='noreferrer'>
+            <img
+              src='/images/logo.svg'
+              className={styles.logo} />
+          </a>
+          <div>
+            <a
+              href='https://savelife.in.ua/'
+              target='_blank'
+              rel='noreferrer'>
+              <img src='/images/social-media-twitter.svg' />
+            </a>
+            <a
+              href='https://savelife.in.ua/'
+              target='_blank'
+              rel='noreferrer'>
+              <img src='/images/social-media-discord.svg' />
+            </a>
+            <a
+              href='https://savelife.in.ua/'
+              target='_blank'
+              rel='noreferrer'>
+              <img src='/images/social-media-instagram.svg' />
+            </a>
+          </div>
+        </div>
+        <div className={styles.content}>
           <img
-            src='/images/logo.svg'
-            className={styles.logo} />
-        </a>
-        <div>
-          <a
-            href='https://savelife.in.ua/'
-            target='_blank'
-            rel='noreferrer'>
-            <img src='/images/social-media-twitter.svg' />
-          </a>
-          <a
-            href='https://savelife.in.ua/'
-            target='_blank'
-            rel='noreferrer'>
-            <img src='/images/social-media-discord.svg' />
-          </a>
-          <a
-            href='https://savelife.in.ua/'
-            target='_blank'
-            rel='noreferrer'>
-            <img src='/images/social-media-instagram.svg' />
-          </a>
+            src='/images/dove-right.svg'
+            className={styles.mobileImage}/>
+          <div className={styles.title}>
+            Unite for peace in Ukraine
+          </div>
+          <div className={styles.subtitle}>
+            Help financially to end the war.
+          </div>
+          <div className={styles.timer}>
+            <Timer date={targetDate} />
+          </div>
+          <LinkButton link='https://savelife.in.ua'>
+            Join our Discord
+          </LinkButton>
+          <img
+            src='/images/dove-right.svg'
+            style={{ transform: 'scaleX(-1) translateX(100%) translateY(' + (-1 * (this.state.scrollOffset || 0)) + '%)'}}
+            className={styles.leftImage} />
+          <img
+            src='/images/dove-right.svg'
+            style={{ transform: 'translateX(100%) translateY(' + (-1 * (this.state.scrollOffset || 0)) + '%)'}}
+            className={styles.rightImage} />
         </div>
       </div>
-      <div className={styles.content}>
-        <img
-          src='/images/dove-right.svg'
-          className={styles.mobileImage}/>
-        <div className={styles.title}>
-          Unite for peace in Ukraine
-        </div>
-        <div className={styles.subtitle}>
-          Help financially to end the war.
-        </div>
-        <div className={styles.timer}>
-          <Timer date={targetDate} />
-        </div>
-        <LinkButton link='https://savelife.in.ua'>
-          Join our Discord
-        </LinkButton>
-        <img
-          src='/images/dove-right.svg'
-          className={styles.leftImage} />
-        <img
-          src='/images/dove-right.svg'
-          className={styles.rightImage} />
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Banner;
--- components/story-block/Story.js
+++ components/story-block/Story.js
@@ -1,44 +1,65 @@
+import React from 'react';
 import styles from './Story.module.scss';
 
-function Story() {
-  return (
-    <div className={styles.Story}>
-      <div className={styles.content}>
-        <div className={styles.title}>
-          Story Behind This Project
-        </div>
-        <div className={styles.subtitle}>
-          February 24th, at 5 a.m
-        </div>
-        <div className={styles.description}>
-          On February 24th, at 5 a.m., all Ukrainians&apos; lives were divided into &quot;before&quot; and &quot;after.&quot; The Russian invasion made the whole Ukraine live in shelters, under the signals of sirens, under inhuman bombardments, and fires in their homeland. The War in Ukraine has been called the largest military conflict in Europe since World War II. This War is not only about Ukraine, it has affected the safety of the entire world.
-        </div>
-        <div className={styles.subtitleAndImage}>
+class Story extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollOffset: 0,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => {
+      const { scrollY, innerHeight } = window;
+      this.setState({
+        scrollOffset: Math.max(((scrollY - 300) / innerHeight) * 100, 0),
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className={styles.Story}>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            Story Behind This Project
+          </div>
           <div className={styles.subtitle}>
-            Ukraine needs EVERYONE’s help
+            February 24th, at 5 a.m
+          </div>
+          <div className={styles.description}>
+            On February 24th, at 5 a.m., all Ukrainians&apos; lives were divided into &quot;before&quot; and &quot;after.&quot; The Russian invasion made the whole Ukraine live in shelters, under the signals of sirens, under inhuman bombardments, and fires in their homeland. The War in Ukraine has been called the largest military conflict in Europe since World War II. This War is not only about Ukraine, it has affected the safety of the entire world.
+          </div>
+          <div className={styles.subtitleAndImage}>
+            <div className={styles.subtitle}>
+              Ukraine needs EVERYONE’s help
+            </div>
+            <img
+              src='/images/heart-mobile.svg'
+              className={styles.mobileImage}/>
+          </div>
+          <div className={styles.description}>
+            You can make a donation! We are more than 30 Ukrainians, and we cannot stand aside, while our home is on fire. We are launching an NFT-project where Everyone can contribute to ending this war. Everyone can help to restore the lives of civilians. Help Ukraine rise from the ashes!
+          </div>
+          <div className={styles.subtitle}>
+            All funds to the War victims
+          </div>
+          <div className={styles.description}>
+            All the money will go to the charity <a href='https://savelife.in.ua/'>https://savelife.in.ua/</a>, the biggest Ukrainian charity foundation, and will only be used for peaceful purposes in Ukraine, to support the Ukrainian army and the victims.
           </div>
           <img
-            src='/images/heart-mobile.svg'
-            className={styles.mobileImage}/>
+            src='/images/heart-left.svg'
+            style={{ transform: 'translateX(-100%) translateY(' + (-1 * (this.state.scrollOffset || 0)) + '%)'}}
+            className={styles.leftImage} />
+          <img
+            src='/images/heart-right.svg'
+            style={{ transform: 'translateY(' + (-1 * (this.state.scrollOffset || 0)) + '%)'}}
+            className={styles.rightImage} />
         </div>
-        <div className={styles.description}>
-          You can make a donation! We are more than 30 Ukrainians, and we cannot stand aside, while our home is on fire. We are launching an NFT-project where Everyone can contribute to ending this war. Everyone can help to restore the lives of civilians. Help Ukraine rise from the ashes!
-        </div>
-        <div className={styles.subtitle}>
-          All funds to the War victims
-        </div>
-        <div className={styles.description}>
-          All the money will go to the charity <a href='https://savelife.in.ua/'>https://savelife.in.ua/</a>, the biggest Ukrainian charity foundation, and will only be used for peaceful purposes in Ukraine, to support the Ukrainian army and the victims.
-        </div>
-        <img
-          src='/images/heart-left.svg'
-          className={styles.leftImage} />
-        <img
-          src='/images/heart-right.svg'
-          className={styles.rightImage} />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Story;
