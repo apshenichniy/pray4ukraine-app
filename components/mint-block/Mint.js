1a0ab6d16e4c938e7fcf724540e274f5771a49d4
Denys Bazanov <dbazanov@deloitte.com>
2022-03-11 15:01:00 +0200

    mint block

--- components/mint-block/Mint.js
+++ components/mint-block/Mint.js
@@ -1,9 +1,39 @@
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import styles from './Mint.module.scss';
 
 function Mint(props) {
   return (
-    <div ref={props.innerRef} className={styles.Mint}>
-      Mint
+    <div
+      ref={props.innerRef}
+      className={styles.Mint}>
+      <div className={styles.background}>
+        <img src='/images/bg.webp' />
+      </div>
+      <div className={styles.overlay}>
+        <div className={styles.popup}>
+          <img src='/images/mint-star-right.png' className={styles.mintStarRight} />
+          <div className={styles.title}>
+            Help Ukraine <br/> with Pray4Ukraine
+          </div>
+          <div className={styles.priceOverline}>
+            MINT PRICE
+          </div>
+          <div className={styles.mintPrice}>
+            <div className={styles.price}>0,08</div>
+            <div className={styles.priceUnit}>SOL</div>
+          </div>
+          <div className={styles.availabilityText}>
+            Max 10 per transaction
+          </div>
+          <WalletMultiButton className={styles.connectButton + ' button'} />
+          <div className={styles.helpTitle}>
+            Still have questions?
+          </div>
+          <div className={styles.helpText}>
+            Check <a href='/'>video instruction</a> or <a href='/'>contact our support</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
