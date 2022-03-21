93daf3db2d6aa4fd95467e592ef0bb19240e25d0
Denys Bazanov <dbazanov@deloitte.com>
2022-03-21 14:49:11 +0200

    remove lorem ipsum

--- components/premium-collection-block/PremiumCollection.js
+++ components/premium-collection-block/PremiumCollection.js
@@ -15,10 +15,10 @@ function PremiumCollection() {
             src='/images/nft-1.webp'
             className={styles.cardImage} />
           <div className={styles.cardTitle}>
-            Inceptos Tellus
+            Mournful Mother
           </div>
           <div className={styles.cardDescription}>
-            Donec id elit non mi porta gravida at eget metus.
+            My art is powerfully emotional cause it’s about suffering from war. This piece symbolises a mourning and crying mother, which one can interpret to be both as a Ukrainian mother or Ukraine as whole, deeply saddened for those who have lost their lives and their loved ones.
           </div>
         </div>
         <div className={styles.card}>
@@ -26,10 +26,10 @@ function PremiumCollection() {
             src='/images/nft-2.webp'
             className={styles.cardImage} />
           <div className={styles.cardTitle}>
-            Fermentum Dapibus
+            Saving lives
           </div>
           <div className={styles.cardDescription}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            I have a little daughter who is inspiring me to live. The war makes me feel unsafe. And I know I can't even find a safe place for my daughter. But there are people who are taking a bigger risk. Its our army, those men and women who fight for us, surfing their lives to save our children. I hope my art will help the superheroes of our country.
           </div>
         </div>
         <div className={styles.card}>
@@ -37,10 +37,11 @@ function PremiumCollection() {
             src='/images/nft-3.webp'
             className={styles.cardImage} />
           <div className={styles.cardTitle}>
-            Ipsum Cursus Fusce
+            Ukraine will stand
           </div>
           <div className={styles.cardDescription}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            I love my country. And it hurts a lot.
+            With a picture, I wanted to show pain, fear, and our resilience, strength. No matter how scary and painful, we will preserve our will, language, culture, we will win. We will stand
           </div>
         </div>
       </div>
