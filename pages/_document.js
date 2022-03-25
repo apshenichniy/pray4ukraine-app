428749b15cfa4e3bd3d49ec7ce3d8531c7a57f46
Denys Bazanov <dbazanov@deloitte.com>
2022-03-25 11:08:39 +0200

    add twitter pixel

--- pages/_document.js
+++ pages/_document.js
@@ -103,6 +103,14 @@ class MyDocument extends Document {
 
             gtag('config', 'G-CVEPZW061W');`}}>
           </script>
+          <script dangerouslySetInnerHTML={{
+            __html: `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            // Insert Twitter Pixel ID and Standard Event data below
+            twq('init','o87q0');
+            twq('track','PageView');`}}>
+          </script>
         </Head>
         <body>
           <Main />
